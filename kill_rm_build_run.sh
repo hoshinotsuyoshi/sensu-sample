c1fd908feebf5f0ed499e0ed66edff5a7f78e11a
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2014-05-06 09:48:37 +0900

    Add i-am-awaking

--- kill_rm_build_run.sh
+++ kill_rm_build_run.sh
@@ -1,3 +1,5 @@
 cont=$(docker ps | grep sensu-sample | cut -d' ' -f1)
 docker kill $cont && docker rm $cont
+echo $RANDOM > no-cache
 docker build -t sensu-sample . && docker run -d -p 15672:15672 -p 8080:8080 -p 4567:4567 sensu-sample
+rm no-cache
--- sensu/plugins/check-i-am-awaking.rb
+++ sensu/plugins/check-i-am-awaking.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 
-exit 0
+exit 1
