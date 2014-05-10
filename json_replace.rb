0d566f8927d3ce755d444e66dc152ce06d9383fb
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2014-05-10 14:43:34 +0900

    Link to graphite

--- json_replace.rb
+++ json_replace.rb
@@ -1,5 +1,6 @@
 require './heroku_variables_load'
 
+# heroku env
 Dir.glob(['/etc/sensu/conf.d/*.json', '/etc/sensu/*.json']).each do |path|
   string = File.read(path)
   ENV.each do |k,v|
@@ -7,3 +8,10 @@ Dir.glob(['/etc/sensu/conf.d/*.json', '/etc/sensu/*.json']).each do |path|
   end
   File.open(path, 'w'){|f| f.write string}
 end
+
+# graphite address
+path = '/etc/sensu/config.json'
+string = File.read(path)
+ip_address = ENV['GRAPHITE_PORT_2003_TCP_ADDR']
+string = string.gsub('$GRAPHITE_IP_ADDRESS', ip_address)
+File.open(path, 'w'){|f| f.write string}
--- kill_rm_build_run.sh
+++ kill_rm_build_run.sh
@@ -1,2 +1,2 @@
 docker kill sensu-server && docker rm sensu-server
-docker build -t hoshinotsuyoshi/sensu-server . && docker run -d -p 15672:15672 -p 8080:8080 -p 4567:4567 -p 5672:5672 --name sensu-server hoshinotsuyoshi/sensu-server
+docker build -t hoshinotsuyoshi/sensu-server . && docker run -d -p 15672:15672 -p 8080:8080 -p 4567:4567 -p 5672:5672 --name sensu-server --link graphite:graphite hoshinotsuyoshi/sensu-server
